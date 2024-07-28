ddcb2247af3118d0e79b27b168f526c852f426b2
kokoball <kbp4154@naver.com>
2024-07-28 11:24:23 +0900

    chore: 프로젝트 시작 설정 완료

---
+++ wordle/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">
+        Hello, Vite + React + TypeScript + Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
