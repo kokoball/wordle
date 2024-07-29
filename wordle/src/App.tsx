48ea269e51f5f126819d88e024b8e680b8f59d87
kokoball <kbp4154@naver.com>
2024-07-29 15:21:46 +0900

    feat: toast ui 설치 및 적용

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalStyle } from '@/styles/GlobalStyle';
 import AppRoutes from './router';
+import { Toaster } from '@/components/ui/toaster';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <AppRoutes />
+      <Toaster />
     </BrowserRouter>
   );
 };
