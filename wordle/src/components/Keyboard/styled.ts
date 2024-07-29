93524f009b386e0bd5d305ea1ded8a8f5a3acc39
kokoball <kbp4154@naver.com>
2024-07-29 12:18:12 +0900

    feat: Keyboard component 및 스타일 추가

---
+++ wordle/src/components/Keyboard/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const KeyboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+`;
