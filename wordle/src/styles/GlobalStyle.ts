756f72fb258807ef7c7c2032214d1378d3bf6f98
kokoball <kbp4154@naver.com>
2024-07-29 11:53:42 +0900

    chore: GlobalStyle.ts 스타일 설정 및 App.css 초기화

---
+++ wordle/src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+    background-color: #ffffff;
+    color: #000000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+`;
