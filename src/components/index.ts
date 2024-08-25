97693339fca520ccbcc5f6fed57ef6648dfd4cd9
kokoball <kbp4154@naver.com>
2024-08-25 21:41:03 +0900

    커밋 정리하기

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Board } from './Board/Board';
-export { default as Key } from './Key/Key';
-export { default as Keyboard } from './Keyboard/Keyboard';
-export { default as CardWithInput } from './CardWithInput';
-export { default as Spacing } from './Spacing';
